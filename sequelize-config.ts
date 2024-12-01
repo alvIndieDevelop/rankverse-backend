6656c5096724a41d278a74db7d10f375330aba40
Alvaro Martin Caballero <alvindiedevelop@gmail.com>
2024-12-01 18:09:36 -0500

    iniciando el backend

---
+++ sequelize-config.ts
@@ -0,0 +1,31 @@
+module.exports = {
+  local: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    uuid_extension: 'uuid-ossp',
+  },
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    uuid_extension: 'uuid-ossp',
+    logging: true,
+    define: {
+      charset: 'utf8mb3',
+      collate: 'utf8mb3_bin',
+    },
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    uuid_extension: 'uuid-ossp',
+  },
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadModels: true,
+      logging: (l) => console.log(l),
+      models: [__dirname + '/../**/*.model.{ts}'],
+      synchronize: false,
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
